666edfb19f6bf52d4cfca0b8bd25809995aff548
dung.nq2 <dung.nq2@amanotes.com>
2022-12-16 10:17:36 +0700

    fix

--- src/pages/scanQrCode/ScanQrCode.tsx
+++ src/pages/scanQrCode/ScanQrCode.tsx
@@ -5,13 +5,18 @@ import {
 } from "react-html5-qrcode-reader";
 
 const idScanContainer = "reader";
-const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+const config = {
+  fps: 10,
+  qrbox: { width: 250, height: 250 },
+  facingMode: "environment",
+};
 
 const html5QrCodeScannerFile = "https://unpkg.com/html5-qrcode";
 
 export default function ScanQrCode() {
   const { Html5QrcodeScanner } = useHtml5QrCodeScanner(html5QrCodeScannerFile);
   const { devices, error } = useAvailableDevices(html5QrCodeScannerFile);
+  const [data, setData] = React.useState();
   console.log({ devices, error });
   //useEffect
 
@@ -22,7 +27,10 @@ export default function ScanQrCode() {
     if (Html5QrcodeScanner) {
       let html5QrcodeScanner = new Html5QrcodeScanner("reader", config, false);
       html5QrcodeScanner.render(
-        (data: any) => console.log("success ->", data),
+        (data: any) => {
+          console.log("success ->", data);
+          setData(data);
+        },
         (err: any) => console.log("err ->", err)
       );
     }
@@ -30,7 +38,8 @@ export default function ScanQrCode() {
 
   return (
     <div className="w-[400px] h-[700px] shadow-lg shadow-gray-400">
-      <div id={idScanContainer} className="w-full h-full"></div>
+      {!data && <div id={idScanContainer} className="w-full h-full"></div>}
+      {data && <div>{data}</div>}
     </div>
   );
 }
